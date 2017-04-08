7a84d9ff55cfa1a868d509305e387d0aead4b5ba
R F <a>
2017-04-07 21:33:44 -0700

    fix pomo counter display on mobile

--- client/src/components/PomoIcon.jsx
+++ client/src/components/PomoIcon.jsx
@@ -4,11 +4,14 @@ import "./PomoIcon.css";
 
 export default function PomoIcon(props) {
   return(
-    <img
-      src="tomato.svg"
-      width="18px"
-      height="18px"
-      className="pomo-img"
-    />
+    <span className="pomo-img-container">
+      <img
+        src="tomato.svg"
+        width="18px"
+        height="18px"
+        className="pomo-img"
+        alt="Pomodoro"
+      />
+    </span>
   )
 }
