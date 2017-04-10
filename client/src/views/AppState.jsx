55a83c0ebb717ac7a8190dd60741043062adfe14
R F <a>
2017-04-09 22:37:21 -0700

    fix work-work transition and hopefully unwanted mobile transitions

--- client/src/views/AppState.jsx
+++ client/src/views/AppState.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import Paper from "material-ui/Paper";
 import { advanceProgram, confirmStateChange, APPSTATE } from "../actions";
 import AppDefault from "./AppDefault";
@@ -11,15 +12,15 @@ import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 import "./AppState.css";
 
-const programStateToComponent = {
-  [APPSTATE.DEFAULT]: <AppDefault />,
-  [APPSTATE.WORK]: <AppWork />,
-  [APPSTATE.REST]: <AppRest />,
-  [APPSTATE.BREAK]: <AppBreak />,
-  [APPSTATE.SETTINGS]: <AppSettings />
-}
+function AppState(props) {
+  const programStateToComponent = {
+    [APPSTATE.DEFAULT]: <AppDefault />,
+    [APPSTATE.WORK]: <AppWork key={props.currentKey} />,
+    [APPSTATE.REST]: <AppRest />,
+    [APPSTATE.BREAK]: <AppBreak />,
+    [APPSTATE.SETTINGS]: <AppSettings />
+  }
 
-export default function AppState(props) {
   return(
     <ReactCSSTransitionGroup
       transitionName="transition"
@@ -28,11 +29,25 @@ export default function AppState(props) {
     >
       <Paper
         className="main-paper"
-        key={Math.random()}
+        key={props.currentKey}
       >
-        {programStateToComponent[props.state]}
+        {programStateToComponent[props.appState]}
       </Paper>
     </ReactCSSTransitionGroup>
   )
 }
 
+function mapStateToProps(state) {
+  let key = state.currentProgramIndex;
+  if(state.appState === APPSTATE.SETTINGS) {
+    key += 0.1;
+  } else if(state.apPState === APPSTATE.DEFAULT) {
+    key += 0.2;
+  }
+  return {
+    appState: state.appState,
+    currentKey: key
+  }
+}
+
+export default connect(mapStateToProps)(AppState);
